6e46b1c918abe8743d54af4e5b6fe7bf626a5ed2
João Pedro Alves <jotalmeida007@hotmail.com>
2020-08-09 15:50:51 -0300

    update git ignore

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,11 +2,11 @@ import express from 'express';
 
 const app = express();
 
-app.get('/users', () => {
+app.get('/users', (req, res) => {
   const users = [
     { name: 'João Pedro', age: 19 },
     { name: 'Gyh', age: 19 },
   ];
+  return res.json(users);
 });
-
 app.listen(3333);
