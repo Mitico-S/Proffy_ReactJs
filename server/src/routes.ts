4cebdf34859a3c5d67074e285513cc00d20379ed
João Pedro Alves <jotalmeida007@hotmail.com>
2020-08-18 19:40:36 -0300

    bug on connection db

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import ClassesController from './controllers/ClassesControler';
+import ConnectionController from './controllers/ConnectionsControllers';
 
 const routes = express.Router();
 const classesControllers = new ClassesController();
+const connectionControler = new ConnectionController();
 
 routes.get('/classes', classesControllers.index);
 routes.post('/classes', classesControllers.create);
+
+routes.post('/connections', connectionControler.create);
 
 export default routes;
