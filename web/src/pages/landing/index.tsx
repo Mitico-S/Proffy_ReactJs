7042058e79d75df4133e3bc98b9bb1d7e7744a56
João Pedro Alves <jotalmeida007@hotmail.com>
2020-08-20 16:04:08 -0300

    connect front and backend

--- web/src/pages/landing/index.tsx
+++ web/src/pages/landing/index.tsx
@@ -1,5 +1,5 @@
 // import react
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 // import logo
 import logoImg from '../../assets/images/logo.svg';
@@ -21,8 +21,18 @@ import './style.css';
 
 // import Link
 import { Link } from 'react-router-dom';
+import api from '../../services/api';
 
 function Landing() {
+  const [totalConnections, setTotalConnections] = useState(0);
+
+  useEffect(() => {
+    api.get('/connections').then((response) => {
+      const { total } = response.data;
+
+      setTotalConnections(total);
+    });
+  }, []);
   return (
     <div id="page-landing">
       <div id="page-landing-content" className="container">
@@ -48,7 +58,7 @@ function Landing() {
         </div>
 
         <span className="total connections">
-          Total de 200 conexões já realizadas{' '}
+          Total de {totalConnections} conexões já realizadas{' '}
           <img src={purpleHeartIcon} alt="coração roxo"></img>
         </span>
       </div>
