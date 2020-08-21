7fd96f24752ee38b195df5d555a5ad7253f14dd3
João Pedro Alves <jotalmeida007@hotmail.com>
2020-08-21 17:03:36 -0300

    add proffy pages

--- mobile/src/pages/routes/AppStack.tsx
+++ mobile/src/pages/routes/AppStack.tsx
@@ -4,6 +4,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import Landing from '../Landing';
 import GiveClasses from '../GiveClasses';
+import Studytabs from './StudyTabs';
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -13,6 +14,7 @@ function AppStack() {
       <Navigator screenOptions={{ headerShown: false }}>
         <Screen name="Landing" component={Landing} />
         <Screen name="GiveClasses" component={GiveClasses} />
+        <Screen name="Study" component={Studytabs} />
       </Navigator>
     </NavigationContainer>
   );
