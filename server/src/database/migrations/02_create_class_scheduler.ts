07745fc41647d62368c89272129ccb808f19edb8
João Pedro Alves <jotalmeida007@hotmail.com>
2020-08-21 00:40:02 -0300

    Revert "Revert "Revert "fixing the backend"""
    This reverts commit 140367f7856055a6065d256420d69f01b7d9ccff.

--- server/src/database/migrations/02_create_class_scheduler.ts
+++ server/src/database/migrations/02_create_class_scheduler.ts
@@ -1,19 +1,20 @@
 import Knex from 'knex';
 
 export async function up(knex: Knex) {
-  return knex.schema.createTable('class_schedule', (table) => {
+  return knex.schema.createTable('class_schedule', table => {
     table.increments('id').primary();
-    table.integer('week_day').notNullable;
-    table.integer('from').notNullable;
-    table.integer('to').notNullable;
 
-    table
-      .integer('class_id')
+    table.integer('week_day').notNullable();
+    table.integer('from').notNullable();
+    table.integer('to').notNullable();
+
+    table.integer('class_id')
       .notNullable()
       .references('id')
-      .inTable('users')
+      .inTable('classes')
       .onUpdate('CASCADE')
       .onDelete('CASCADE');
+
   });
 }
 
--- server/src/utils/convertHourToMinutes.ts
+++ server/src/utils/convertHourToMinutes.ts
@@ -1,6 +1,6 @@
 export default function convertHourToMinutes(time: string): number {
-  const [hour, minutes] = time.split(':').map(Number);
+  //08:40
+  const [ hour, minutes ] = time.split(':').map(Number);
   const timeInMinutes = (hour * 60) + minutes;
-
   return timeInMinutes;
 }
