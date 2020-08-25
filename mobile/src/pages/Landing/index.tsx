1f137621b19b756c661ceb1e92ee78782bb63524
João Pedro Alves <jotalmeida007@hotmail.com>
2020-08-25 11:59:02 -0300

    connect api

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Image, Text, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import styles from './styles';
@@ -7,8 +7,18 @@ import studyIcon from '../../assets/images/icons/study.png';
 import giveClassesIcon from '../../assets/images/icons/give-classes.png';
 import heartIcon from '../../assets/images/icons/heart.png';
 import { RectButton } from 'react-native-gesture-handler';
+import api from '../../services/api';
 
 function Landing() {
+  const [totalConnections, setTotalConnections] = useState(0);
+
+  useEffect(() => {
+    api.get('/connections').then((response) => {
+      const { total } = response.data;
+
+      setTotalConnections(total);
+    });
+  }, []);
   const { navigate } = useNavigation();
 
   function handleNavigateToGiveClassesPage() {
@@ -48,7 +58,7 @@ function Landing() {
       </View>
 
       <Text style={styles.totalConnections}>
-        Total de 285 conexões já realizadas {''}
+        Total de {totalConnections} conexões já realizadas {''}
         <Image source={heartIcon} />
       </Text>
     </View>
