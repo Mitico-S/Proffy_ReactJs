95655f86ec3759ca75d26436748a91b1c7e2aa07
João Pedro Alves <jotalmeida007@hotmail.com>
2020-10-08 14:01:01 -0300

    Revert "finish project"
    This reverts commit 853dc89435b7c5d6062ec43cdac7406c60da92a0.

--- mobile/src/pages/Favorites/index.tsx
+++ mobile/src/pages/Favorites/index.tsx
@@ -1,36 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { View, ScrollView } from 'react-native';
-import AsyncStorage from '@react-native-community/async-storage';
-import { useFocusEffect } from '@react-navigation/native';
-
-import PageHeader from '../../components/PageHeader';
-import TeacherItem, { Teacher } from '../../components/TeacherItem';
-
 import styles from './styles';
+import PageHeader from '../../components/PageHeader';
+import TeacherItem from '../../components/TeacherItem';
 
 function Favorites() {
-  const [favorites, setFavorites] = useState([]);
-
-  function loadFavorites() {
-    AsyncStorage.getItem('favorites').then((response) => {
-      if (response) {
-        const favoritedTeachers = JSON.parse(response);
-
-        setFavorites(favoritedTeachers);
-      }
-    });
-  }
-
-  useFocusEffect(
-    React.useCallback(() => {
-      loadFavorites();
-    }, [])
-  );
-
   return (
     <View style={styles.container}>
       <PageHeader title="Meus proffys favoritos" />
-
       <ScrollView
         style={styles.teacherList}
         contentContainerStyle={{
@@ -38,12 +15,13 @@ function Favorites() {
           paddingBottom: 16,
         }}
       >
-        {favorites.map((teacher: Teacher) => {
-          return <TeacherItem key={teacher.id} teacher={teacher} favorited />;
-        })}
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
       </ScrollView>
     </View>
   );
 }
-
 export default Favorites;
